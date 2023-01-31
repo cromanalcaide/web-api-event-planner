b9e2a2a3aa0d9774f1b36b46d2ad5a1ecb27b9e4
cromanalcaide <cromanalcaide@gmail.com>
2023-01-31 20:36:26 +0000

    first models created

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,19 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import ForeignKey
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    phone = db.Column(db.Integer, unique=False)
+    city = db.Column(db.String(120), nullable=False)
+    country = db.Coumn(db.String(120), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,3 +24,23 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Events(db.Model):
+    __tablename__ = "events"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(120), nullable=False)
+    time = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(360), nullable=False)
+    location = db.Column(db.String(240), nullable=False)
+    guests = db.Column(db.String(240), nullable=False)
+    image = db.Column(db.String(360), nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey("user.id"))
+    user = relationship()
+
+
+class Contacts(db.Model):
+    __tablename_ = "contacts"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.Integer, primary_key=True)
+    user_id = 
