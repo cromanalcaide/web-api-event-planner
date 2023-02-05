4fc1304e2b95d0f1a84571a4c395ef3f5c44a94e
lbdelilla <lbdelilla@gmail.com>
2023-02-05 16:38:31 +0000

    navbar almost done

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "../../styles/navbar.css"
+
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+			<div className="container-fluid">
+				<p className="navbar-brand">ComMeet</p>
+				<div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+					<div className="navbar-nav">
+						<Link to="/" className="link">
+							<span className="nav-link" aria-current="page" href="#">HOME</span>
+							{/* <hr className="nav-line border border-2 opacity-100" /> */}
+						</Link>
+						<Link to="" className="link">
+							<span className="nav-link" href="#">¿QUÉ ES COMMEET?</span>
+						</Link>
+						<Link to="/contact" className="link">
+							<span className="nav-link" href="#">CONTACTO</span>
+						</Link>
+						<Link to="/login" className="link">
+							<span className="nav-link">LOGIN/REGISTRO</span>
+						</Link>
+					</div>
 				</div>
 			</div>
 		</nav>
