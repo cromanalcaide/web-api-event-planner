6f84721b2b0b27f9aea337a43c865ed90b73d34d
lbdelilla <lbdelilla@gmail.com>
2023-02-05 18:04:28 +0000

    home page ready

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,17 @@
 import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+// import { Context } from "../store/appContext";
+import { Jumbotron } from "../component/jumbotron";
+import { Description } from "../component/description";
+
 
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+	// const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+		<div>
+			<Jumbotron />
+			<Description />
 		</div>
 	);
 };
