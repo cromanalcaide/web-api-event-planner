79df035a75d7bf66776d0ef708292bd38c13085a
cromanalcaide <cromanalcaide@gmail.com>
2023-02-08 19:00:04 +0000

    server opened

--- src/api/models.py
+++ src/api/models.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -37,7 +35,6 @@ class Events(db.Model):
     time = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(360), nullable=False)
     location = db.Column(db.String(240), nullable=False)
-    guests = db.Column(db.String(240), nullable=False)
     image = db.Column(db.String(360), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship(User)
@@ -55,15 +52,15 @@ class Events(db.Model):
         "time": self.time,
         "description": self.description,
         "location": self.location,
-        "guests": self.guests,
         "image": self.image,
-        "user_id": self.user_id,
+        "user_id": self.user_id
     }
 
 class Contacts(db.Model):
     __tablename__ = 'contacts'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=False, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship(User)
 
@@ -81,6 +78,8 @@ class Contacts(db.Model):
 class Event_Guests(db.Model):
     __tablename__ = 'event_guests'
     id = db.Column(db.Integer, primary_key=True)
+    contact_id = db.Column(db.Integer, db.ForeignKey('contacts.id'))
+    contact = db.relationship(Contacts)
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
     event = db.relationship(Events)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  
@@ -92,30 +91,7 @@ class Event_Guests(db.Model):
     def serialize(self):
         return {
         "id": self.id,
-        "contact_email": self.contact_email,
-        "contact": self.contact,
+        "contact_id": self.contact_id,
         "event_id": self.event_id,
-        "event": self.event,
-        "user_id": self.user_id,
-    }
-
-class Contact_Forms(db.Model):
-    __tablename__='contact_forms'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-    message = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  
-    user = db.relationship(User)
-
-    def __repr__(self):
-        return f'<Contact_Forms {self.id}>'  
-
-    def serialize(self):
-        return {
-        "id": self.id,
-        "name": self.name,
-        "email": self.email,
-        "message": self.message,
         "user_id": self.user_id,
     }
