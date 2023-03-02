001c2d0e6652193f78fbb94bc182b36e043d1510
lbdelilla <lbdelilla@gmail.com>
2023-03-02 12:54:44 +0000

    now if the user is loggued in he can go directly to private from the navbar 'Dashboard'

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useContext } from 'react';
+import { Context } from "../store/appContext";
 
 import "../../styles/navbar.css"
 
 export const Navbar = () => {
 
 	const [activeButton, setActiveButton] = useState("")
+	const { store } = useContext(Context);
 
 	const handleClick = (id) => {
 		setActiveButton(id)
@@ -20,33 +23,40 @@ export const Navbar = () => {
 						<path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
 					</svg></span>
 				</button>
-			<div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-				<div className="navbar-nav">
-					<ul className="nav-ul navbar-nav ">
-						<li id="nav-li-1" className="active">
-							<Link to="/" className="nav-link">
-								<span className="nav-item">INICIO</span>
-							</Link>
-						</li>
-						<li id="nav-li-2" className='active'>
-							<Link to="/about" className="nav-link">
-								<span className="nav-item">¿QUÉ ES COMMEET?</span>
-							</Link>
-						</li>
-						<li id="nav-li-3" className='active'>
-							<Link to="/contact" className="nav-link">
-								<span className="nav-item">CONTACTO</span>
-							</Link>
-						</li>
-						<li id="nav-li-4" className='active'>
-							<Link to="/login" className="nav-link">
-								<span className="nav-item">INGRESO/REGISTRO</span>
-							</Link>
-						</li>
-					</ul>
+				<div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+					<div className="navbar-nav">
+						<ul className="nav-ul navbar-nav ">
+							<li id="nav-li-1" className="active">
+								<Link to="/" className="nav-link">
+									<span className="nav-item">INICIO</span>
+								</Link>
+							</li>
+							<li id="nav-li-2" className='active'>
+								<Link to="/about" className="nav-link">
+									<span className="nav-item">¿QUÉ ES COMMEET?</span>
+								</Link>
+							</li>
+							<li id="nav-li-3" className='active'>
+								<Link to="/contact" className="nav-link">
+									<span className="nav-item">CONTACTO</span>
+								</Link>
+							</li>
+							{!store.user ? (
+								<li id="nav-li-4" className='active'>
+									<Link to="/login" className="nav-link">
+										<span className="nav-item">INGRESO/REGISTRO</span>
+									</Link>
+								</li>) : (
+								<li id="nav-li-4" className='active'>
+									<Link to="/private" className="nav-link">
+										<span className="nav-item">DASHBOARD</span>
+									</Link>
+								</li>
+							)}
+						</ul>
+					</div>
 				</div>
 			</div>
-		</div>
 		</nav >
 	);
 };
